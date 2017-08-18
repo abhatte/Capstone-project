94dd187ca36b6bb0bf4838488ef3599b553b04bf
Ashwini Bhatte <ashwinibhatte@Devendras-MacBook-Pro.local>
2017-08-18 16:20:06 +0200

    file_name=patient_num, master=df, newmaster=clean_df

--- source/diabetes.R
+++ source/diabetes.R
@@ -32,32 +32,32 @@ for(i in 1:70) {
 class(master)
 summary(master)
 
-# create newmaster to clean the data
+# create clean_df to clean the data
 ## remove "000" & "3A" from bg_conc as they are illogical values
-newmaster <- subset(master, bg_conc != "000" & bg_conc != "3A")
-# newmaster contains code "4", "36, & "56", but since there is no entry of such codes in
+clean_df <- subset(master, bg_conc != "000" & bg_conc != "3A")
+# clean_df contains code "4", "36, & "56", but since there is no entry of such codes in
 # the data description, these values are removed as well
-newmaster <- subset(newmaster, code != "4" & code != "36" & code != "56")
+clean_df <- subset(clean_df, code != "4" & code != "36" & code != "56")
 
-#For further EDA, convert newmaster$bg_conc from character to numeric
-newmaster$bg_conc <- as.numeric(newmaster$bg_conc)
+#For further EDA, convert clean_df$bg_conc from character to numeric
+clean_df$bg_conc <- as.numeric(clean_df$bg_conc)
 
 #remove NAs from above
-newmaster <- na.omit(newmaster)
+clean_df <- na.omit(clean_df)
 
 #clean data csv
-write.csv(newmaster, file = "../data wrangling/diabetes_clean.csv")
+write.csv(clean_df, file = "../data wrangling/diabetes_clean.csv")
 
 #check for NA = 8 NAs introduced by coercion in bg_conc
-summary(newmaster)
-glimpse(newmaster)
-length(unique(newmaster$code))
-class(newmaster)
+summary(clean_df)
+glimpse(clean_df)
+length(unique(clean_df$code))
+class(clean_df)
 
 # Since bg_conc is the only numeric variable in the data set; 
 # it’s distribution is observed by plotting the histogram.
 
-qplot(newmaster$bg_conc,
+qplot(clean_df$bg_conc,
       geom="histogram",
       binwidth = 5,  
       main = "Histogram for Blood Glucose", 
@@ -76,40 +76,40 @@ qplot(newmaster$bg_conc,
 
 
 #Code vs Blood Glucose concentration
-ggplot(newmaster, aes(factor(code), bg_conc)) +
+ggplot(clean_df, aes(factor(code), bg_conc)) +
   geom_boxplot() +
   labs(x = "Code", y = "BG concentration (mg/dl)") +
   ggtitle("Relation between Code and Blood Glucose concentration", subtitle = "Figure 2")
 
-summary(newmaster$bg_conc)
+summary(clean_df$bg_conc)
 
 #To gain a better perspective at this, let’s plot the graph of BG concentration vs code by 
 #grouping the codes into 3 sub categories.
 
 # group 1: code 33 - 35 represents codes for type of insulin dose
 c1 <- c(33:35)
-newmaster1 <- newmaster[newmaster$code %in% c1,]
-ggplot(newmaster1, aes(factor(code), bg_conc)) +
+clean_df1 <- clean_df[clean_df$code %in% c1,]
+ggplot(clean_df1, aes(factor(code), bg_conc)) +
   geom_boxplot() +
   labs(x = "Code", y = "BG concentration (mg/dl)") +
   ggtitle("Relation between Insulin dose and Blood Glucose concentration", subtitle = "Figure 3")
 
-summary(newmaster1$bg_conc)
+summary(clean_df1$bg_conc)
 
 # group 2: code 48, 57 - 64 represents meal
 c2 <- c(48, 57:64)
-newmaster2 <- newmaster[newmaster$code %in% c2,]
-ggplot(newmaster2, aes(factor(code), bg_conc)) +
+clean_df2 <- clean_df[clean_df$code %in% c2,]
+ggplot(clean_df2, aes(factor(code), bg_conc)) +
   geom_boxplot() +
   labs(x = "Code", y = "BG concentration (mg/dl)") +
   ggtitle("Relation between Insulin dose and Blood Glucose concentration", subtitle = "Figure 4")
 
-summary(newmaster2$bg_conc)
+summary(clean_df2$bg_conc)
 
 #group 3: code 65 - 72 represents exercise
 c3 <- c(65:72)
-newmaster3 <- newmaster[newmaster$code %in% c3,]
-ggplot(newmaster3, aes(factor(code), bg_conc)) +
+clean_df3 <- clean_df[clean_df$code %in% c3,]
+ggplot(clean_df3, aes(factor(code), bg_conc)) +
   geom_boxplot() +
   labs(x = "Code", y = "BG concentration (mg/dl)") +
   ggtitle("Relation between exercise and Blood Glucose concentration", subtitle = "Figure 5")
@@ -117,12 +117,12 @@ ggplot(newmaster3, aes(factor(code), bg_conc)) +
 # around 0 mg/dl. But, for the remaining code, it is not logical. Hence we assume that these 
 # values were supposed to be measured but they are just the place holder for now.
 
-summary(newmaster3$bg_conc)
+summary(clean_df3$bg_conc)
 
 #Since time is a continuous variable, it will be difficult to explore the data when time is 
 #plotted on x- axis. Which is why the time intervals of 2 hours (time_grp) were created
 
-newmaster <- newmaster %>%
+clean_df <- clean_df %>%
   mutate(time_grp = gsub("^00:[0-9][0-9]:00|^01:[0-9][0-9]:00", "00-02", x = time))%>%
   mutate(time_grp = gsub("^02:[0-9][0-9]:00|^03:[0-9][0-9]:00", "02-04", x = time_grp))%>%
   mutate(time_grp = gsub("^04:[0-9][0-9]:00|^05:[0-9][0-9]:00", "04-06", x = time_grp))%>%   
@@ -141,24 +141,31 @@ newmaster <- newmaster %>%
 # To study the effects, 3 groups are createrd
 #bg_level: 0-80mg/dl = Hypoglycaemic, 81-199mg/dl = Average, above 200mh/dl = Hyperglycemic
 
-newmaster <- newmaster %>%
+clean_df <- clean_df %>%
   mutate(bg_symp = gsub("^([0-9]|[0-7][0-9]|80)$", "Hypoglycemia", x = bg_conc))%>%
   mutate(bg_symp = gsub("^(8[1-9]|9[0-9]|1[0-9][0-9])$", "Average", x = bg_symp))%>%
   mutate(bg_symp = gsub("^(2[0-9][0-9]|3[0-9][0-9]|4[0-9][0-9]|50[0-1])$", 
                         "Hyperglycemia", x = bg_symp)) %>%
   mutate(bg_symp = gsub("^(1.5|2.5|3.5|4.5|6.5|7.5)$", "Hypoglycemia", x = bg_symp))
 
+clean_df <- clean_df %>%
+  mutate(symp_num = gsub("^([0-9]|[0-7][0-9]|80)$", 0, x = bg_conc))%>%
+  mutate(symp_num = gsub("^(8[1-9]|9[0-9]|1[0-9][0-9])$", 1, x = symp_num))%>%
+  mutate(symp_num = gsub("^(2[0-9][0-9]|3[0-9][0-9]|4[0-9][0-9]|50[0-1])$", 
+                        2, x = symp_num)) %>%
+  mutate(symp_num = gsub("^(1.5|2.5|3.5|4.5|6.5|7.5)$", 0, x = symp_num))
+
 
 # The distribution of BG measurements across various time intervals is explored using a simple
 # bar plot.
-ggplot(newmaster, aes(time_grp)) +
+ggplot(clean_df, aes(time_grp)) +
  geom_bar() +
  labs(x = "Hours") +
  ggtitle("Relation between Insulin dose and Blood Glucose concentration", subtitle = "Figure 6")
 
   
 # Relationship between time interval and BG concentration box plot.
-ggplot(newmaster, aes(factor(time_grp), bg_conc)) +
+ggplot(clean_df, aes(factor(time_grp), bg_conc)) +
   geom_boxplot() +
   labs(x = "Hours", y = "Blood glucose concentration") +
   ggtitle("Relation between Insulin dose and Blood Glucose concentration", subtitle = "Figure 7")
@@ -166,7 +173,7 @@ ggplot(newmaster, aes(factor(time_grp), bg_conc)) +
 
 # minimum number of measurements taken from 00 - 04 in the night. 
 #The maximum number of BG concentration are taken during the day time.
-ggplot(newmaster, aes(factor(time_grp), bg_conc)) +
+ggplot(clean_df, aes(factor(time_grp), bg_conc)) +
   geom_point(alpha = 0.1) +
   scale_shape(1, solid = FALSE) +
   geom_jitter(width = 0.1) +
@@ -178,15 +185,15 @@ ggplot(newmaster, aes(factor(time_grp), bg_conc)) +
 # see the distribution of Hypoglycemia, average BG concentration and Hyperglycemia.
 # Each point here represents number of Blood glucose measurements by the patients in 24 hours
 # for several weeks or months.
-ggplot(newmaster, aes(factor(time_grp), bg_conc, col = bg_symp)) +
+ggplot(clean_df, aes(factor(time_grp), bg_conc, col = bg_symp)) +
   geom_point() +
   ggtitle("Distribution of BG symptoms over 24 hours", subtitle = "Figure 9")
 
 
 # working plot Add facet layer
-newmaster$bg_symp <- factor(newmaster$bg_symp, levels = c("Hypoglycemia", "Average",
+clean_df$bg_symp <- factor(clean_df$bg_symp, levels = c("Hypoglycemia", "Average",
                                                           "Hyperglycemia"))
-ggplot(newmaster, aes(bg_symp, bg_conc, col = bg_symp)) +
+ggplot(clean_df, aes(bg_symp, bg_conc, col = bg_symp)) +
   geom_point() +
   geom_boxplot(alpha = 0.4, width = 4.5) +
   facet_grid(.~ (bg_symp)) +
@@ -194,7 +201,7 @@ ggplot(newmaster, aes(bg_symp, bg_conc, col = bg_symp)) +
   ggtitle("Distribution of BG symptoms over 24 hours", subtitle = "Figure 10")
 
 # # boxplot with overlaid scatterplot
-# ggplot(newmaster, aes(factor(time_grp), bg_conc)) +
+# ggplot(clean_df, aes(factor(time_grp), bg_conc)) +
 #   geom_point(alpha = 0.1) +
 #   scale_shape(1, solid = FALSE) +
 #   geom_boxplot(alpha = 0.4) +
@@ -208,16 +215,33 @@ ggplot(newmaster, aes(bg_symp, bg_conc, col = bg_symp)) +
 # 58 - 64       meal (pre - post)
 # 69 - 71       exercise
 
-newmaster <- newmaster %>%
+clean_df <- clean_df %>%
   mutate(type = gsub("^(3[3-5])$", "insulin", x = code)) %>%
   mutate(type = gsub("^(5[8-9]|6[0-4])$", "meal", x = type)) %>%
   mutate(type = gsub("^(6[9]|7[0-1])$", "exercise", x = type))
 
 # To see the comparison between these 3 types of activities, let's plot a box plot
 
-ggplot(subset(newmaster,type %in% c("insulin", "meal", "exercise"))) + 
+ggplot(subset(clean_df,type %in% c("insulin", "meal", "exercise"))) + 
   geom_boxplot(aes(factor(type), bg_conc, group=type, col=type)) +
   labs(x = "Type of activity", y = "Blood glucose concentration, mg/dl") +
   ggtitle("Comparison between type of activity and BG concentration", subtitle = "Figure 11")
 
+# Install required packages
+#install.packages("caTools")
+library(caTools)
 
+set.seed(144)
+
+clean_df$symp_num <- as.numeric(clean_df$symp_num)
+
+split <- sample.split(clean_df$symp_num, SplitRatio = 0.7)
+train <- subset(clean_df, split == TRUE)
+test <- subset(clean_df, split == FALSE)
+
+
+sympLog <- glm(symp_num~bg_conc, data = train)
+summary(sympLog)
+
+predictTest <- predict(sympLog, type = "response", newdata = test)
+table(test$bg_symp, predictTest > 0.5)
